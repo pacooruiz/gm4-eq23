0f5fdf6f3815949d1380a8062a045eb6ca6b8c5e
pacooruiz <franciscoruizroldan2014@gmail.com>
2020-12-17 14:08:31 +0100

    main

---
+++ src/main.cc
@@ -0,0 +1,129 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int trabajador;
+
+    cout<<"\n1. Acceso a administrativos.";
+    cout<<"\n2. Acceso a monitores.";
+    cout<<"\nAcceder a: ";
+    cin>>trabajador;
+
+    if(trabajador==1){
+
+        int option;
+
+        do{
+
+            cout<<"\nMENU\n";
+            cout<<"\n1. Obtener información.\n";
+            cout<<"\n2. Mostrar rutas";
+            cout<<"\n3. Crear ruta.\n";
+            cout<<"\n4. Añadir visitante.";
+            cout<<"\n5. Eliminar visitante.\n";
+            cout<<"\n6. Mostrar senderos.";
+            cout<<"\n7. Actualizar sendero\n";
+            cout<<"\n8. Mostrar monitores.\n";
+            cout<<"\n9. Mostrar incidencias.\n";
+            cout<<"\n10. Salir.\n";
+
+            cout<<"\nIntroduzca su elección: ";
+            cin>>option;
+
+            switch (option){
+            case 1:
+                /* code */
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 6:
+
+                break;
+
+            case 7:
+
+                break;
+
+            case 8:
+
+                break;
+
+            case 9:
+
+                break;
+            }
+
+        }while(option!=10);
+
+        return 0;
+
+    }else if(trabajador==2){
+
+        int option;
+
+        do{
+            cout<<"\nMENU\n";
+            cout<<"\n1. Mostrar mis rutas.";
+            cout<<"\n2. Modificar ruta.\n";
+            cout<<"\n3. Mostrar visitantes.";
+            cout<<"\n4. Mostrar senderos.\n";
+            cout<<"\n5. Añadir incidencia.\n";
+            cout<<"\n6. Salir.\n";
+
+            cout<<"\nIntroduzca su elección: ";
+            cin>>option;
+
+            switch (option){
+            case 1:
+                /* code */
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 4:
+
+                break;
+
+            case 5:
+
+                break;
+
+            case 6:
+
+                break;
+            }
+
+        }while(option!=6);
+
+        return 0;
+
+    }else{
+
+        cout<<"\nError. Elección no permitida.\n";
+        return 0;
+
+    }
+}
